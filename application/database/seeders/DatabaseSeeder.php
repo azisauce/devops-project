de69f28ffb6c0f8654666f7cb689284c9746051c
unknown <turkiazizu012@gmail.com>
2024-05-10 15:17:38 +0100

    fixing the application

--- application/database/seeders/DatabaseSeeder.php
+++ application/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,39 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Task::factory(20)->create([
+            'taskcreator_id' => User::factory(),
+            'assigneduser_id' => User::factory()
         ]);
-    }
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@password.com',
+            'password' => bcrypt('password'),
+            'admin' => 1
+        ]);
+
+        User::create([
+            'name' => 'jane',
+            'email' => 'janedoe@email.com',
+            'password' => bcrypt('password')
+        ]);
+    
+    }     
+       
 }
